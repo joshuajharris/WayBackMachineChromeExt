2d4c6f92e96f935f911df6abde4f0e968022329e
joshuajharris <joshua.jharris13@gmail.com>
2015-03-12 21:43:00 -0400

    Puts a link in popup to closest wayback snapshot of current tab to user defined date

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var bkg = chrome.extension.getBackgroundPage();
+
 /*
 *
 * gotCurrentTabUrl() from google official chrome extension tutorial
@@ -18,7 +20,6 @@ function getCurrentTabUrl(callback) {
 
     callback(url);
   });
-
 }
 
 /*
@@ -28,12 +29,21 @@ function getCurrentTabUrl(callback) {
 *
 */
 function getWayBackRes(url, timestamp, callback) {
-  var searchUrl = 'http://archive.org/wayback/available?url=' + encodeURIComponent(url) + '&timestamp=' + encodeURIComponent(timestamp);
-  //can add a timestamp by appending '&timestamp=:timestamp' to search url
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open('GET', searchUrl, false);
+  // sanitize url, remove query info
+  url = url.substr(0, url.indexOf('?'));
+
+  // bkg.getBackgroundPage().console.log(url);
+  
+  // append tab url to the wayback api
+  var searchUrl = 'http://archive.org/wayback/available?url=' + url + '&timestamp=' + timestamp;
+  // can add a timestamp by appending '&timestamp=:timestamp' to search url
+  
+  var xmlHttp = new XMLHttpRequest();   // create xmlHttpRequest object
+  xmlHttp.open('GET', searchUrl, false);// set POST/GET, url, and async
   xmlHttp.send(null);
-  callback(xmlHttp.responseText);
+
+  // bkg.console.log(JSON.parse(xmlHttp.responseText));
+  callback(JSON.parse(xmlHttp.responseText));   // return JSON object from wayback api
   
 }
 /*
@@ -42,23 +52,45 @@ function getWayBackRes(url, timestamp, callback) {
 * statusText
 *
 */
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function renderWayBackLink(link) {
+  //document.getElementById('status').textContent = statusText;
+  var content = '<a id="wbLink" href="' + link + '">' + 'link</a>'; // popup a tag link
+  document.getElementById('wayBackLink').innerHTML = content; // put link on popup
+  
+  // give link a listener to open link in new tab on click
+  document.getElementById('wbLink').addEventListener('click', function(){
+    chrome.tabs.create({ url: link });
+  }, false);
+
+
 }
 
+function renderStatus(statusText) {
+  document.getElementById('status').innerText = statusText;
+}
+//on tab load summon satan
 document.addEventListener('DOMContentLoaded', function() {
-  var tabUrl;
+  var tabUrl;       // duh
+  var wayBackRes;   // JSON response from the wayback api
+ 
+  //return the url of the active tab 
   getCurrentTabUrl(function(url){
     tabUrl = url;
-    renderStatus(url);
+    renderStatus(tabUrl);   // outputs a message to the user in popup
   });
 
+  //when the user chooses a date...
   document.getElementById('datePicker').addEventListener('change', function(){
-    var time = document.getElementById('datePicker').value;
-    var time = time.toString().replace(/-/g,'');
-    getWayBackRes(tabUrl, time, function(data){
-      renderStatus(data);
+    var time = document.getElementById('datePicker').value;   // simply get value from date input element
+    var time = time.toString().replace(/-/g,'');    // remove hyphons from time string
+    
+    // gets the wayback object from wayback api
+    getWayBackRes(tabUrl, time, function(res) {
+      wayBackRes = res;
+      renderWayBackLink(res.archived_snapshots.closest.url);  // put link in popup 
+      //bkg.console.log(res.archived_snapshots); 
     });
   });
+
 });
 
